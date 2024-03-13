5e3593877b210b2b4148ca3b553076a6c2477306
Raj-kumarSingh <rajs93138@gmail.com>
2024-03-14 00:23:32 +0530

    Modified BUBLE_SORT

--- SORTNG/BUBLE_SORT.java
+++ SORTNG/BUBLE_SORT.java
@@ -3,18 +3,37 @@ package SORTNG;
 public class BUBLE_SORT {
     public static void main(String[] args) {
         int  a[]={5,3,4,6,7};
-        buble(a);
+        //buble(a);
+        Modified_bubleSort(a);
         printarr(a);
     }
-    public static void buble(int a[]) {
+//    public static void buble(int a[]) {
+//        for (int turn = 0; turn < a.length - 1; turn++) {
+//            for (int j = 0; j < a.length - 1 - turn; j++) {
+//               // if (a[j] > a[j + 1])
+//                if (a[j] < a[j + 1]){
+//                    int temp = a[j];
+//                    a[j] = a[j + 1];
+//                    a[j + 1] = temp;
+//                }
+//            }
+//        }
+//    }
+    public static void Modified_bubleSort(int a[]) {
         for (int turn = 0; turn < a.length - 1; turn++) {
+            boolean swapped=false;
             for (int j = 0; j < a.length - 1 - turn; j++) {
-               // if (a[j] > a[j + 1])
+                // if (a[j] > a[j + 1])
                 if (a[j] < a[j + 1]){
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
+                    swapped=true;
                 }
+            }
+            if(swapped==false)
+            {
+                break;
             }
         }
     }
