5ff7bcbb0378ecfe1bd588687658ad3f93929679
Raj-kumarSingh <rajs93138@gmail.com>
2024-04-23 22:17:10 +0530

    LinkedList

--- LINKED_LIST/LinkedList.java
+++ LINKED_LIST/LinkedList.java
@@ -1,4 +1,4 @@
-package LINKED_LIST;
+ package LINKED_LIST;
 
 public class LinkedList {
     public static class Node {
@@ -162,6 +162,74 @@ public class LinkedList {
             }
             head=prev;
         }
+        public void deleteNthfromEnd(int n)
+        {
+            int sz=0;
+            Node temp=head;
+            while(temp!=null)
+            {
+                temp=temp.next;
+                sz++;
+            }
+            if(n==sz)
+            {
+                head=head.next;
+                return;
+            }
+            int i=1;
+            int iToFind=sz-n;
+            Node prev=head;
+            while (i<iToFind)
+            {
+                prev=prev.next;
+                i++;
+            }
+            prev.next=prev.next.next;
+            return;
+        }
+        // SLOW FAST APPROACH
+    public Node findMid(Node head) //helper
+    {
+        Node slow=head;
+        Node fast=head;
+        while (fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow; // slow is my mid
+    }
+    public boolean checkPalindrome()
+    {
+        if(head==null || head.next==null)
+        {
+            return true;
+        }
+        // step1 - Find mid
+        Node midNode=findMid(head);
+        Node prev=null;
+        Node curr=midNode;
+        Node next;
+        while (curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node right=prev;
+        Node left=head;
+        while (right!=null)
+        {
+            if(left.data!=right.data)
+            {
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         LinkedList l1=new LinkedList();
         l1.addFirst(2);
@@ -169,17 +237,25 @@ public class LinkedList {
         l1.addLast(4);
         l1.addLast(5);
         l1.addMiddle(2,3);
-        l1.removeFirst();
+//        l1.removeFirst();
+//        l1.print();
+//        System.out.println(l1.size);
+//        l1.removeLast();
+//        l1.print();
+//        System.out.println(l1.size);
+//        System.out.println(l1.itSearch(4));
+//        System.out.println(l1.itSearch(10));
+//        System.out.println(l1.recursiveSearch(4));
+//        l1.reverse();
+//         l1.print();
+//         l1.deleteNthfromEnd(3);
+//         l1.print();
+        l1.addLast(1);
+        l1.addLast(2);
+        l1.addLast(1);
         l1.print();
-        System.out.println(l1.size);
-        l1.removeLast();
-        l1.print();
-        System.out.println(l1.size);
-        System.out.println(l1.itSearch(4));
-        System.out.println(l1.itSearch(10));
-        System.out.println(l1.recursiveSearch(4));
-        l1.reverse();
-        l1.print();
+        System.out.println(l1.checkPalindrome());
     }
+
 
     }
